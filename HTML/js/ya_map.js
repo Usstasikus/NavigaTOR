9baf430800aba38c2db66958bdd9968519f9aa3c
Stas Don <usstasikus@gmail.com>
2017-10-19 22:39:22 +0300

    Added some kind of table to map

--- HTML/js/ya_map.js
+++ HTML/js/ya_map.js
@@ -3,16 +3,8 @@ ymaps.ready(function() {
     map = new ymaps.Map('map', {
         center: [55.751574, 37.573856],
         zoom: 9,
-        controls: []
+        controls: ['searchControl']
     }, {
         searchControlProvider: 'yandex#search'
     });
-});
-
-map.controls.add( control.SearchControl, {
-    float: 'none',
-    position: {
-        top: 10,
-        left: 10
-    }
 });
