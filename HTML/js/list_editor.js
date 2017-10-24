e954be4a51a3b95fcc8896b6f5b4071fe30d712e
Stas Don <usstasikus@gmail.com>
2017-10-24 23:24:38 +0300

    Добавлена функция добавления строчек в лист

--- HTML/js/list_editor.js
+++ HTML/js/list_editor.js
@@ -1,49 +1,24 @@
-
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+var placesList = document.getElementById("placesList");
+// var input = document.getElementById("placesInput");
+function newElement(e, input) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) {
+        var listElement = document.createElement("li");
+        listElement.className = "list-group-item";
+        var text = document.createTextNode(input.value);
+        listElement.appendChild(text);
+        placesList.appendChild(listElement);
+        input.value = ""
+    }
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.parentElement.remove(i);
-  }
-}
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  alert("Ничего не реализовано!!");
-  // var li = document.createElement("li");
-  // var inputValue = document.getElementById("myInput").value;
-  // var t = document.createTextNode(inputValue);
-  // li.appendChild(t);
-  // if (inputValue === '') {
-  //   alert("You must write something!");
-  // } else {
-  //   document.getElementById("myUL").appendChild(li);
-  // }
-  // document.getElementById("myInput").value = "";
-  //
-  // var span = document.createElement("SPAN");
-  // var txt = document.createTextNode("\u00D7");
-  // span.className = "close";
-  // span.appendChild(txt);
-  // li.appendChild(span);
-  //
-  // for (i = 0; i < close.length; i++) {
-  //   close[i].onclick = function() {
-  //     var div = this.parentElement;
-  //     div.remove();
-  //   }
-  // }
-}
+// localStorage["placesList"] = document.getElementById("placesList"));
+// localStorage.setItem("i", 0)
+// function newElement(){
+//   var i = localStorage.getItem("i");
+//   var placesList = localStorage["placesList"];
+//   listElement.className = "list-group-item";
+//   var text = document.createTextNode(i);
+//   listElement.appendChild(text);
+//   placesList.appendChild(listElement);
+// }
